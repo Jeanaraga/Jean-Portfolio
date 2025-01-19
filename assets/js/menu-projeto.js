a6c4f8cce7a2171484a493b71b5c0602f087febc
Jean Pedro <jeanjesuspedrobook@gmail.com>
2025-01-19 17:12:22 -0300

    Atualiza informações pessoais e projetos no portfólio

--- assets/js/menu-projeto.js
+++ assets/js/menu-projeto.js
@@ -1,32 +1,32 @@
 export function menuProjeto() {
     const textos = {
         p1: {
-          titulo: "No Bullying: ",
+          titulo: "No Bullying",
           descricao:
             "Projeto escolar; é um site contra o bullying, desenvolvido para a instituição Proz. O projeto foi escolhido entre todas as matrizes da Proz. O site contém informações sobre o assunto e uma aba para fazer denúncias.",
           imagem: "./assets/img/nobullyng.PNG",
           link: "https://jeanaraga.github.io/Bully/"
         },
         p2: {
-          titulo: "SB Filmes: ",
+          titulo: "Snap",
           descricao:
-            "Projeto escolar; é um site de gerenciamento de filmes, provavelmente o site mais complexo que já fiz. Há opções de cadastro e login usando localStorage, além de opções para curtir e descurtir filmes.",
-          imagem: "./assets/img/sbfilmes.PNG",
-          link: "",
+            "Este projeto é uma landing page simples desenvolvida para uma loja virtual, com um design moderno e menu mobile interativo. O site foi criado para oferecer uma experiência de navegação fluida, garantindo acessibilidade e usabilidade em dispositivos móveis. A landing page é focada em apresentar produtos e facilitar a interação do usuário com a loja.",
+          imagem: "./assets/img/desktop-preview.jpg",
+          link: "https://github.com/Jeanaraga/intro-section-with-dropdown-navigation-main",
         },
         p3: {
-          titulo: "Jogo da Memória: ",
+          titulo: "Jogo da Memória ",
           descricao:
-            "Projeto escolar; o conhecido jogo da memória, mas com figuras relacionadas à enfermagem. Desenvolvemos este jogo em homenagem ao Dia da Enfermagem, com quatro grupos: se acertar, continua, se perder, passa para o próximo grupo. O projeto foi muito elogiado pela direção da Proz.",
+            "O conhecido jogo da memória, mas com figuras relacionadas à enfermagem. Desenvolvemos este jogo em homenagem ao Dia da Enfermagem, com quatro grupos: se acertar, continua, se perder, passa para o próximo grupo. O projeto foi muito elogiado pela direção da Proz.",
           imagem: "./assets/img/jogodamemoria.PNG",
           link:"",
         },
         p4: {
-          titulo: "Calculadora de Gorjeta: ",
+          titulo: "Sunnyside ",
           descricao:
-            "Projeto pessoal; uma calculadora de gorjeta em que você pode definir por quantas pessoas vai dividir a conta, a porcentagem de gorjeta para o garçom e o valor total da conta. O resultado mostra quanto cada pessoa deve pagar.",
-          imagem: "./assets/img/calculadora.png",
-          link:""
+            "Este projeto é uma landing page desenvolvida para um site de marketing pessoal, projetada para destacar habilidades, serviços e conquistas de forma atrativa e profissional. Com um design moderno e responsivo, a página inclui seções interativas, como portfólio, depoimentos e um formulário de contato, oferecendo uma experiência de navegação intuitiva e funcional para visitantes em qualquer dispositivo.",
+          imagem: "./assets/img/desktop-preview (1).jpg",
+          link:"https://jeanaraga.github.io/Pagina-de-Destino-Agencia/"
         },
       };
   
